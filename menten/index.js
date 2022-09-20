dfcdd5074aa64994dabcb8e720c537713f165703
twotter <d2maw5@gmail.com>
2022-09-20 13:02:45 +0500

    logout nav

--- menten/index.js
+++
@@ -1,8 +0,0 @@
-function getLocalTime(){
-  let date = new Date();
-  let indo = date.getHours() + 7
-  let times = indo+":"+date.getMinutes();
-  return times;
-  
-  }
-  console.log(getLocalTime())
